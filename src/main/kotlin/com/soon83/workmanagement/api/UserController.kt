93386c8a452af5553745da50fff899f47a192798
ds <2601948@gmail.com>
2022-03-18 18:27:35 +0900

    HttpUtil 생성

--- src/main/kotlin/com/soon83/workmanagement/api/UserController.kt
+++ src/main/kotlin/com/soon83/workmanagement/api/UserController.kt
@@ -1,15 +1,13 @@
 package com.soon83.workmanagement.api
 
-import com.soon83.workmanagement.domain.User
 import com.soon83.workmanagement.dto.UserCreateDto
 import com.soon83.workmanagement.dto.UserResponseDto
 import com.soon83.workmanagement.service.UserCreateService
 import com.soon83.workmanagement.service.UserQueryService
+import com.soon83.workmanagement.utils.HttpUtil.Companion.getCurrentUri
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
-import java.net.URI
 import javax.validation.Valid
 
 @RestController
@@ -49,15 +47,5 @@ class UserController(
         return ResponseEntity
             .created(getCurrentUri(userResponseDto.id!!))
             .body(userResponseDto)
-    }
-
-    fun getCurrentUri(id: Long): URI {
-        val path = "/$id"
-        val requestUri = ServletUriComponentsBuilder.fromCurrentRequest().toUriString()
-        log.debug("# requestUri: $requestUri")
-        log.debug("# requestUri.indexOf: ${requestUri.indexOf("/", 8)}")
-        val uri = URI(requestUri.substring(requestUri.indexOf("/", 8)) + path)
-        log.debug("# uri: $uri")
-        return uri
     }
 }
---
+++ src/main/kotlin/com/soon83/workmanagement/utils/HttpUtil.kt
@@ -0,0 +1,20 @@
+package com.soon83.workmanagement.utils
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import java.net.URI
+
+class HttpUtil {
+
+    companion object {
+
+        @JvmStatic fun getCurrentUri(id: Long): URI {
+            val path = "/$id"
+            val requestUri = ServletUriComponentsBuilder.fromCurrentRequest().toUriString()
+            //log.debug("# requestUri: $requestUri")
+            //log.debug("# requestUri.indexOf: ${requestUri.indexOf("/", 8)}")
+            return URI(requestUri.substring(requestUri.indexOf("/", 8)) + path)
+            //log.debug("# uri: $uri")
+            //return uri
+        }
+    }
+}
